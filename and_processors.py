cca1c64fd46ee2937aeaae93071e90daa80c2319
Kiril Lazarov <kirillazarov@abv.bg>
2022-03-27 12:32:06 +0300

    init

---
+++ and_processors.py
@@ -0,0 +1,17 @@
+from math import floor
+
+processors_count = int(input())
+people_count = int(input())
+days_count = int(input())
+
+work_hours = people_count * days_count * 8
+done_processors = floor(work_hours / 3)
+profit = done_processors * 110.1
+diff = abs(profit - processors_count * 110.1)
+if done_processors >= processors_count:
+    print(f"Profit: -> {diff:.2f} BGN")
+
+else:
+    print(f"Losses: -> {diff:.2f} BGN")
+
+
