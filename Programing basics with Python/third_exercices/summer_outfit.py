48b96ff90af832ec1de206db5a97b75df23e5ab9
Kiril Lazarov <kirillazarov@abv.bg>
2022-04-28 19:14:35 +0300

    Task

---
+++ Programing basics with Python/third_exercices/summer_outfit.py
@@ -0,0 +1,35 @@
+degrees = int(input())
+time = input()
+outfit = ''
+shoes = ''
+
+if time == 'Morning':
+    if 10 <= degrees <= 18:
+        outfit = 'Sweatshirt'
+        shoes = 'Sneakers'
+    elif 18 < degrees <= 24:
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
+    else:
+        outfit = 'T-Shirt'
+        shoes = 'Sandals'
+elif time == 'Afternoon':
+    if 10 <= degrees <= 18:
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
+    elif 18 < degrees <= 24:
+        outfit = 'T-Shirt'
+        shoes = 'Sandals'
+    else:
+        outfit = 'Swim Suit'
+        shoes = 'Barefoot'
+elif time == 'Evening':
+    outfit = 'Shirt'
+    shoes = 'Moccasins'
+# else:
+#     outfit = outfit
+#     shoes = shoes
+
+print(f"It's {degrees} degrees, get your {outfit} and {shoes}.")
+# time == 'Evening':
+# time == 'Evening':
