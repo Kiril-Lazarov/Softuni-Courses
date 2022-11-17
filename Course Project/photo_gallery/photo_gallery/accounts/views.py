19ea9c0859143ecd2849b722627432f64f1d31bd
Kiril Lazarov <kirillazarov@abv.bg>
2022-11-17 23:22:11 +0200

    Changes in views.

--- Course Project/photo_gallery/photo_gallery/accounts/views.py
+++ Course Project/photo_gallery/photo_gallery/accounts/views.py
@@ -1,34 +1,24 @@
 from . import forms
-from django.http import HttpResponseRedirect
+
 from django.urls import reverse_lazy
 from django.views import generic as views
 from django.contrib.auth import views as auth_views, get_user_model, authenticate, login
-from django.shortcuts import render, redirect
-from django.views.generic import FormView
 
-from photo_gallery.accounts.forms import UserCreateForm
 from photo_gallery.photos.models import BasePhotos
 
 UserModel = get_user_model()
 
 
-def get_profile(pk):
-    try:
-        return UserModel.objects.filter(pk=pk).get()
-    except UserModel.DoesNotExist as ex:
-        return None
-
-
-class UserSignIn(auth_views.LoginView):
+class UserSignInView(auth_views.LoginView):
     template_name = 'accounts/login-page.html'
     success_url = reverse_lazy('index')
 
 
-class UserSignOut(auth_views.LogoutView):
+class UserSignOutView(auth_views.LogoutView):
     next_page = reverse_lazy('index')
 
 
-class UserSignUp(views.CreateView):
+class UserSignUpView(views.CreateView):
     template_name = 'accounts/register-page.html'
     form_class = forms.UserCreateForm
     success_url = reverse_lazy('index')
@@ -43,9 +33,7 @@ class UserSignUp(views.CreateView):
         return to_return
 
 
-
-
-class UserDetails(views.DetailView):
+class UserDetailsView(views.DetailView):
     template_name = 'accounts/profile.html'
     model = UserModel
 
@@ -57,10 +45,10 @@ class UserDetails(views.DetailView):
         return context
 
 
-class UserEdit(views.UpdateView):
+class UserEditView(views.UpdateView):
     template_name = 'accounts/edit-profile.html'
     model = UserModel
-    fields = ('username', 'email', 'first_name', 'last_name', 'gender')
+    fields = ('username', 'profile_picture', 'email', 'first_name', 'last_name', 'gender')
 
     def get_success_url(self):
         return reverse_lazy('user profile', kwargs={
@@ -68,19 +56,19 @@ class UserEdit(views.UpdateView):
         })
 
 
-class UserDelete(views.DeleteView):
+class UserDeleteView(views.DeleteView):
     template_name = 'accounts/delete-profile.html'
     model = UserModel
     fields = '__all__'
     success_url = reverse_lazy('index')
 
 
-class UserGallery(views.DetailView):
+class UserGalleryView(views.DetailView):
     template_name = 'accounts/profile-gallery.html'
     model = UserModel
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        context['photos'] = BasePhotos.objects.filter(user_id=self.request.user.pk)
+        context['users'] = UserModel.objects.all()
+        context['photos'] = reversed(BasePhotos.objects.filter(user_id=self.request.user.pk))
         return context
