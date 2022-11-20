4ca21d4b3993dc20eb2b7c76778fb7a56b784a46
Kiril Lazarov <kirillazarov@abv.bg>
2022-11-20 19:19:48 +0200

    Added add_photo_comment and edit_photo_comment.

--- Course Project/photo_gallery/photo_gallery/photos/views.py
+++ Course Project/photo_gallery/photo_gallery/photos/views.py
@@ -1,22 +1,131 @@
 from django.shortcuts import render, redirect
-from django import forms
+from django.urls import reverse
 
-from photo_gallery.photos.forms import UploadPhotoForm
-from photo_gallery.photos.models import BasePhotos
+from photo_gallery.accounts.models import AppUser
+from photo_gallery.accounts.views import UserModel
+from photo_gallery.photos.forms import UploadPhotoForm, EditPhotoForm, DeletePhotoForm, UploadAstroPhotoForm, \
+    UploadStreetPhotoForm, UploadPortraitPhotoForm, AddCommentForm, EditCommentForm
+from photo_gallery.photos.models import BasePhotos, PhotoComments
 
 
-def UploadPhoto(request):
-    photos = BasePhotos.objects.all()
+class PhotoCategoryFactory:
+    category_form = {
+        'astro_photography': UploadAstroPhotoForm,
+        'portrait': UploadPortraitPhotoForm,
+        'street': UploadStreetPhotoForm,
+    }
+
+    @classmethod
+    def get_photo_category_form(cls, photo_category, request=None):
+        print(cls.category_form)
+        return cls.category_form[photo_category](request)
+
+    # @classmethod
+    # def get_photo_category_model(cls, photo_category):
+
+
+
+
+def upload_photo_view(request):
     if request.method == 'GET':
         form = UploadPhotoForm()
     else:
         form = UploadPhotoForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('user profile', pk=request.user.pk)
+        photo = form.save(commit=False)
+        photo_category = photo.category
+        photo_category_form = PhotoCategoryFactory.get_photo_category_form(photo_category, request.POST)
+        photo_assessment = photo_category_form.save(commit=False)
+        if form.is_valid() and photo_category_form.is_valid():
+            photo.user_id = request.user.pk
+            photo.save()
+            form.save_m2m()
+            photo_assessment.user_id = request.user.pk
+            photo_assessment.photo_id = photo.pk
+            photo_assessment.save()
+            photo_category_form.save_m2m()
+            return redirect('profile gallery', pk=request.user.pk)
 
     context = {
         'form': form,
-        'photos': photos,
     }
     return render(request, 'photos/upload-photos.html', context)
+
+
+def photo_details_view(request, slug):
+    photo = BasePhotos.objects.filter(slug=slug).get()
+    comments = PhotoComments.objects.all()
+
+    context = {
+        'photo': photo,
+        'comments': comments,
+    }
+    return render(request, 'accounts/../../templates/photos/photo-details.html', context)
+
+
+def photo_edit_view(request, slug):
+    photo = BasePhotos.objects.filter(slug=slug).get()
+    if request.method == 'GET':
+        form = EditPhotoForm(instance=photo)
+    else:
+        form = EditPhotoForm(request.POST, instance=photo)
+        if form.is_valid():
+            form.save()
+            return redirect('profile gallery', pk=request.user.pk)
+    context = {
+        'form': form,
+        'photo': photo,
+    }
+
+    return render(request, 'photos/photo-edit.html', context)
+
+
+def delete_photo_view(request, slug):
+    photo = BasePhotos.objects.filter(slug=slug).get()
+    user = UserModel.objects.filter(pk=photo.user_id).get()
+    if request.method == 'GET':
+        form = DeletePhotoForm(instance=photo)
+    else:
+        form = DeletePhotoForm(request.POST, instance=photo)
+        if form.is_valid():
+            form.save()
+            return redirect('user profile', pk=user.pk)
+    context = {
+        'form': form,
+        'user': user,
+        'photo': photo,
+    }
+
+    return render(request, 'photos/photo-delete.html', context)
+
+def add_photo_comment(request,pk, slug):
+
+    photo = BasePhotos.objects.filter(slug=slug).get()
+    form = AddCommentForm(request.POST)
+    body = form.data['body']
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.photo_id = photo.id
+        comment.user_id = pk
+        comment.comment = body
+        comment.save()
+
+    return redirect('details photo')
+
+def edit_photo_comment(request, pk, slug):
+    photo = BasePhotos.objects.filter(slug=slug).get()
+    comment = PhotoComments.objects.filter(user_id=pk, photo_id=photo.id).get()
+    if request.method == "GET":
+        form = EditCommentForm(instance=comment)
+    else:
+
+        form = EditCommentForm(request.POST, instance=comment)
+        success_url = reverse('details photo')
+        if form.is_valid():
+            form.save()
+            return redirect(success_url)
+    context = {
+        'comment': comment
+    }
+
+    return render(request, 'photos/photo-details.html', context)
