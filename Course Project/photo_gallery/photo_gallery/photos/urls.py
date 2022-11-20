651044daa05baf2fc4b82d14f7d99fcc3e6751fb
Kiril Lazarov <kirillazarov@abv.bg>
2022-11-20 19:21:49 +0200

    Added urls for photos.

--- Course Project/photo_gallery/photo_gallery/photos/urls.py
+++ Course Project/photo_gallery/photo_gallery/photos/urls.py
@@ -1,9 +1,16 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
 
-from photo_gallery.photos.views import UploadPhoto
+from django.urls import path, include
+
+from photo_gallery.photos.views import upload_photo_view, photo_details_view, photo_edit_view, delete_photo_view, \
+    add_photo_comment, edit_photo_comment
 
 urlpatterns = (
-    path('upload/', UploadPhoto, name='upload photo'),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,)
+    path('upload/', upload_photo_view, name='upload photo'),
+    path('details/<slug:slug>/', photo_details_view, name='details photo'),
+    path('edit/<slug:slug>/', photo_edit_view, name='edit photo'),
+    path('delete/<slug:slug>/', delete_photo_view, name='delete photo'),
+    path('comment/', include([
+        path('add/<int:pk>/<slug:slug>/', add_photo_comment, name='add photo comment'),
+        path('edit/<int:pk>/<slug:slug>/', edit_photo_comment, name='edit photo comment'),
+    ])),
+)
