aaf3ce43238577f616454047abb8779dd16bb0e4
Kiril Lazarov <kirillazarov@abv.bg>
2022-11-22 22:33:33 +0200

    Added classes BasePhotoAdmin, PhotoCommentsAdmin, AstroPhotographyAssessmentAdmin, PortraitPhotographyAssessment and StreetPhotographyAssessment

--- Course Project/photo_gallery/photo_gallery/photos/admin.py
+++ Course Project/photo_gallery/photo_gallery/photos/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from photo_gallery.photos.models import PhotoComments, AstroPhotographyAssessment, PortraitPhotographyAssessment, \
+    StreetPhotographyAssessment
+
+
+@admin.register(PhotoComments)
+class PhotoCommentsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user_id', 'photo_id', 'comment',)
+
+
+@admin.register(AstroPhotographyAssessment)
+class AstroPhotographyAssessmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PortraitPhotographyAssessment)
+class PortraitPhotographyAssessment(admin.ModelAdmin):
+    pass
+
+@admin.register(StreetPhotographyAssessment)
+class StreetPhotographyAssessment(admin.ModelAdmin):
+    pass
