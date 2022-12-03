6d4f975c4ba5a8833f95088eb1b4f17c9710c779
Kiril Lazarov <kirillazarov@abv.bg>
2022-12-03 22:00:51 +0200

    Added OwnerRequired mixin.

--- Course Project/photo_gallery/photo_gallery/utils.py
+++ Course Project/photo_gallery/photo_gallery/utils.py
@@ -1,6 +1,7 @@
 import random
 import string
-
+from django.views import generic as views
+from django.shortcuts import redirect
 from django.utils.text import slugify
 
 
@@ -12,7 +13,7 @@ def unique_slug_generator(instance, new_slug=None):
     if new_slug is not None:
         slug = new_slug
     else:
-        slug = slugify(instance)
+        slug = slugify(instance.title)
 
     Klass = instance.__class__
     slug_exists = Klass.objects.filter(slug=slug).exists()
@@ -20,3 +21,15 @@ def unique_slug_generator(instance, new_slug=None):
         new_slug = f'{slug}-{random_string_generator(size=4)}'
         return unique_slug_generator(instance, new_slug=new_slug)
     return slug
+
+
+class OwnerRequired(views.UpdateView):
+    def get(self, request, *args, **kwargs):
+        result = super().get(request, *args, **kwargs)
+        print(request.user.pk)
+        print(self.object.id)
+        if request.user.pk == self.object.id:
+            return result
+        else:
+            return redirect('index')
+
