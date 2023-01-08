d74d2b411f956a9bdb90e1ec5b9c6d51f42771b1
Kiril Lazarov <kirillazarov@abv.bg>
2023-01-09 01:51:40 +0200

    Add tests for char_validator in AppUser model.

--- Course Project/photo_gallery/tests/accounts/account_views/profile_tests.py
+++ Course Project/photo_gallery/tests/accounts/account_views/profile_tests.py
@@ -1,24 +1,55 @@
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from photo_gallery.accounts.models import AppUser
-from photo_gallery.accounts.views import UserEditView
+from photo_gallery.accounts.models import Gender
+
+UserModel = get_user_model()
 
 
 class ProfileModelTests(TestCase):
-    #Tests chars_validator when user inputs first (or last) name which contains only letters
-    def test_chars_validator(self):
-        profile = AppUser(
-            username='Koko',
-            email='koko@abv.bg',
-            password='qwert',
-            gender='Do not show',
+    # Tests chars_validator when user inputs first (or last) name which contains one non letter char, expected
+    # to raise ValidationError
+    def test_chars_validator_with_non_letter_char(self):
+        username = "Koko"
+        email = 'koko@abv.bg'
+        gender = Gender.choices()[0][0]
+        password = 'qwert'
+
+        user = UserModel(
+            username=username,
+            email=email,
+            password=password,
+            gender=gender,
         )
 
         first_name_of_user = 'Kokoko1'
-        profile.first_name = first_name_of_user
+        user.first_name = first_name_of_user
 
         with self.assertRaises(ValidationError) as er:
-            profile.save()
+            user.full_clean()
+            user.save()
             self.assertEqual('Name must content only letters.', str(er.exception))
-        # self.assertEqual(profile.first_name, first_name_of_user)
+            self.assertEqual(user.first_name, first_name_of_user)
+
+    # Tests char_validator with correct input
+    def test_chars_validator_with_correct_chars(self):
+        username = "Koko"
+        email = 'koko@abv.bg'
+        gender = Gender.choices()[0][0]
+        password = 'qwert'
+
+        user = UserModel(
+            username=username,
+            email=email,
+            password=password,
+            gender=gender,
+        )
+
+        first_name_of_user = 'Kokoko'
+        user.first_name = first_name_of_user
+
+        user.full_clean()
+        user.save()
+
+        self.assertEqual(user.first_name, first_name_of_user)
