ccbd04c433c9ff484e2f47a0b5f2c05679909fc0
Kiril Lazarov <kirillazarov@abv.bg>
2023-01-31 18:01:48 +0200

    Add test_initial_assessments_values and test_assessments_values_for_editing.

--- Course Project/photo_gallery/tests/photos/test_photo_details.py
+++ Course Project/photo_gallery/tests/photos/test_photo_details.py
@@ -37,7 +37,7 @@ class PhotoDetailsTest(UserPhotosMixin, TestCase):
         self.client.get(reverse_lazy('details photo', kwargs={'slug': photo_to_check.slug}))
         self.assertEqual(self.logged_user.pk, photo_to_check.user_id)
 
-    # Verifies that the user can rate other user's photos when current photo is not yet rate by him
+    # Verifies that the user can rate other user photo when the photo is not yet rated by him
     def test_logged_user_permission_to_rate_other_user_photos(self):
         photo_to_check = self.other_user_photos[0]
         self.client.login(username=self.logged_user.username, password='user1pass')
@@ -45,7 +45,19 @@ class PhotoDetailsTest(UserPhotosMixin, TestCase):
         self.assertNotEqual(self.logged_user.pk, photo_to_check.user_id)
         self.assertEqual(response.context_data['is_rated_from_user'], 'Rate')
 
-    # Verifies that the user can rate other user's photos when current photo is not yet rate by him
+    # Verifies that initial logged user assessments values for the photo are equal to zero
+    def test_initial_assessments_values(self):
+        photo_to_check = self.other_user_photos[0]
+        self.client.login(username=self.logged_user.username, password='user1pass')
+        response = self.client.get(reverse_lazy('photo assessment', kwargs={'slug': photo_to_check.slug}))
+        assessment_values_list = []
+
+        # Take needed assessment values from response.context and append them to list
+        for i in range(17, 39, 5):
+            assessment_values_list.append(int(response.context[i]["widget"]["value"]))
+        self.assertEqual(sum(assessment_values_list), 0)
+
+    # Verifies that the user can rate other user photo when the photo is already rated by him
     def test_logged_user_already_rate_other_user_photo(self):
         photo_to_check = self.logged_user_photos[0]
         AstroPhotographyAssessment.objects.create(
@@ -67,3 +79,33 @@ class PhotoDetailsTest(UserPhotosMixin, TestCase):
 
         self.assertEqual(self.logged_user.pk, assessed_photo.assessing_user)
         self.assertEqual(response.context_data['is_rated_from_user'], 'Edit rate')
+
+    # Verifies correct assessments values already given by logged user for particular photo
+    def test_assessments_values_for_editing(self):
+        photo_to_check = self.logged_user_photos[0]
+        AstroPhotographyAssessment.objects.create(
+            photo_id=photo_to_check.pk,
+            owner_id=self.other_user.pk,
+            rating1=0,
+            contrast=6,
+            detail=3,
+            focus=2,
+            light=2,
+            color_temperature=6,
+            assessing_user=self.logged_user.pk,
+        )
+
+        logged_user_assessments_items = {
+            'light': 2,
+            'focus': 2,
+            'detail': 3,
+            'contrast': 6,
+            'color_temperature': 6,
+        }
+        self.client.login(username=self.logged_user.username, password='user1pass')
+
+        response = self.client.get(reverse_lazy('photo assessment', kwargs={'slug': photo_to_check.slug}))
+        response_context_items = {}
+        for i in range(17, 39, 5):
+            response_context_items[response.context[i]["widget"]["name"]] = int(response.context[i]["widget"]["value"])
+        self.assertEqual(logged_user_assessments_items, response_context_items)
