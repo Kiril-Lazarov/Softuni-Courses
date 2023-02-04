d3bb731ebeadf7be0a6c70593c650510efa936c8
Kiril Lazarov <kirillazarov@abv.bg>
2023-02-05 00:32:11 +0200

    Add test.

--- Course Project/photo_gallery/tests/photos/test_photo_comments.py
+++ Course Project/photo_gallery/tests/photos/test_photo_comments.py
@@ -3,12 +3,13 @@ from django.urls import reverse_lazy
 
 from photo_gallery.photos.models import PhotoComments
 from tests.photos.test_photo_details import UserPhotosMixin
+from django.template.response import TemplateResponse
 
 
-class PhotoCommentsTest(UserPhotosMixin,TestCase):
+class PhotoCommentsTest(UserPhotosMixin, TestCase):
     @staticmethod
     def create_comment(user_pk, photo_id, comment):
-        return PhotoComments.objects.create(user_id=user_pk, photo_id=photo_id,comment=comment)
+        return PhotoComments.objects.create(user_id=user_pk, photo_id=photo_id, comment=comment)
 
     def setUp(self) -> None:
         self.logged_user = self.create_user(username='user1', password='user1pass')
@@ -20,8 +21,8 @@ class PhotoCommentsTest(UserPhotosMixin,TestCase):
                                                        photo_id=self.logged_user_photos[0].pk,
                                                        comment='This is a logged user`s comment!')
         self.other_user_comment = self.create_comment(user_pk=self.other_user.pk,
-                                                       photo_id=self.other_user_photos[0].pk,
-                                                       comment='This is a other user`s comment!')
+                                                      photo_id=self.other_user_photos[0].pk,
+                                                      comment='This is a other user`s comment!')
 
     # Verifies that `edit comment` and `delete comment` are not available options for non logged user
     def test_non_logged_user_can_only_read_comments(self):
@@ -30,3 +31,11 @@ class PhotoCommentsTest(UserPhotosMixin,TestCase):
         self.assertFalse('edit comment' in str(photo_data.content))
         self.assertFalse('delete comment' in str(photo_data.content))
 
+    # Verifies that `edit comment` and `delete comment` are not available options for logged
+    # user on other user photo
+    def test_logged_user_cannot_modify_other_user_comments(self):
+        photo_to_check = self.other_user_photos[0]
+        self.client.login(username=self.logged_user.username, password='user1pass')
+        photo_data = self.client.get(reverse_lazy('details photo', kwargs={'slug': photo_to_check.slug}))
+        self.assertFalse('edit comment' in str(photo_data.content))
+        self.assertFalse('delete comment' in str(photo_data.content))
