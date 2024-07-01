e4d49be6c56e5993bad76307434c5abdd883e062
Kiril Lazarov <kirillazarov@abv.bg>
2024-07-01 22:53:34 +0300

    Task

---
+++ Python OOP/OOP/inheritance_lection/multilevel_inheritance/project/car.py
@@ -0,0 +1,6 @@
+from task_for_classes.project import Vehicle
+
+
+class Car(Vehicle):
+    def drive(self):
+        return 'driving...'
---
+++ Python OOP/OOP/inheritance_lection/multilevel_inheritance/project/sports_car.py
@@ -0,0 +1,11 @@
+from task_for_classes.project import Car
+
+
+class SportsCar(Car):
+    def race(self):
+        return 'racing...'
+
+a = SportsCar()
+
+
+
