f02510cb6466c5e244dba0356d2f538bf3d83ca4
Kiril Lazarov <kirillazarov@abv.bg>
2024-07-08 21:03:40 +0300

    Task

---
+++ Python OOP/OOP/inheritance_lection/project/fruit.py
@@ -0,0 +1,7 @@
+from task_for_classes.project import Food
+
+
+class Fruit(Food):
+    def __init__(self, name, expiration_date):
+        super().__init__(expiration_date)
+        self.name = name
