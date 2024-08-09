397c5cc6ce00e764ad9d4091d32aebcdb4f24495
Kiril Lazarov <kirillazarov@abv.bg>
2024-08-09 23:12:40 +0300

    Task

---
+++ Python OOP/OOP/inheritance_exercices/need_for_speed/project/race_motorcycle.py
@@ -0,0 +1,7 @@
+from task_for_classes.project import Motorcycle
+
+
+class RaceMotorcycle(Motorcycle):
+    DEFAULT_FUEL_CONSUMPTION = 8
+    def __init__(self, fuel, horse_power):
+        super().__init__(fuel, horse_power)
